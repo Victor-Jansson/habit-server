c7e6dcac111e45c1274e1704db8fef344cb3d72b
Victor Jansson <victor@kaching.mobi>
2018-07-02 21:50:02 +0200

    adding ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+      name: "habit",
+      script: "index.js"
+    }],
+    deploy: {
+      production: {
+        key: "/Users/victor/victor2.pem",
+        user: "ec2-user",
+        host: ["34.236.148.171"],
+        ref: "origin/master",
+        repo: "git@github.com:Victor-Jansson/habit-server.git",
+        path: "/home/ec2-user/server",
+        'ssh_options': 'ForwardAgent=yes',
+        "post-deploy": "npm install --production && pm2 startOrReload ecosystem.config.js"
+      },
+    }
+  }
